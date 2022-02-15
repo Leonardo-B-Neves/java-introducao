917cd7119d56f9e4f4ef3e13883c68dd0c01bb30
atileo0123 <leo012.n4@gmail.com>
2022-02-15 15:02:05 -0300

    media de pequenas partes de um vetor

---
+++ mediaComJanela/src/MediaComJanela.java
@@ -0,0 +1,34 @@
+public class MediaComJanela {
+    public static void main(String[] args) {
+        int X = 5;
+        int Y = 2;
+        int notas[] = {7, 8, 4, 1, 6};
+        float medias[] = media(notas, X, Y);
+        printaVetor(medias);
+
+    }
+
+    public static float[] media (int vetorDados[], int X, int Y){
+
+        int aux = X - Y + 1;
+        float vetorResultante[] = new float[aux];
+        int somaJanela = 0;
+        for (int i = 0; i <= X - Y; i++) {
+            for (int i2 = 0; i2 < Y; i2++) {
+                somaJanela += vetorDados[i + i2];
+            }
+            vetorResultante[i] = somaJanela / Y;
+            somaJanela = 0;
+        }
+        return vetorResultante;
+    }
+
+    public static void printaVetor (float vetor[]){
+        System.out.print("[  ");
+        for(float i:vetor){
+            System.out.print(i + "  ");
+        }
+        System.out.print("]");
+    }
+
+}
